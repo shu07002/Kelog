4d8cd51f0dd6027b3de8dbd6602cbb2c8c02ac06
shu07002@gmail.com <shu07002@gmail.com>
2024-08-12 15:41:53 +0900

    feat: 네비게이션 바 반응형 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./common/Header";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
+import NavigationBar from "./common/NavigationBar";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <NavigationBar />
         <Router />
       </BrowserRouter>
     </div>
---
+++ src/common/NavigationBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../styles/navigationBar/navigationBar.scss";
+
+const NavigationBar = () => {
+  return (
+    <nav className="navigation">
+      <section className="nav-section">
+        <div className="nav-section-category">
+          <h4>트렌딩</h4>
+          <h4>최신</h4>
+          <h4>피드</h4>
+        </div>
+      </section>
+      <section>
+        <div className="nav-option">
+          <select name="timeline">
+            <option>오늘</option>
+            <option>이번 주</option>
+            <option>이번 달</option>
+            <option>올해</option>
+          </select>
+          <img
+            className="nav-bars-icon"
+            src="icons/bars-solid.svg"
+            alt="menu"
+          />
+        </div>
+      </section>
+    </nav>
+  );
+};
+
+export default NavigationBar;
